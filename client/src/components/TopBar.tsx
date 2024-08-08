194e7992f37161e22d2e992fa99ac150cac6c648
unknown <dracokd19@gmail.com>
2024-08-08 14:32:44 +0545

    added profile picute at user profile button

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { logout } from "../features/users/authSlice";
 import { useDispatch } from "react-redux";
 import { useNavigate } from 'react-router-dom';
-
+import { baseUrl } from "../config/constants";
+import { assets } from "../assets/assets";
 function TopBar() {
+
+  const [profileImage,setProfileImage] = useState('');
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
   const dispatch = useDispatch();
   const nagivate = useNavigate();
 
@@ -16,30 +20,46 @@ function TopBar() {
   }
   const handleUserIconClick = () => {
     setShowOptions(!showOptions);
+    console.log(profileImage);
   };
+
+  useEffect(() => {
+    setProfileImage(user._id ? `${baseUrl}profile/profileImage/${user._id}` : assets.profile);
+  }, [user._id, baseUrl]);
+
 
   return (
     <>
-      <div className="nav bg-black h-10 text-white w-full flex justify-end items-center border-b">
-        <div className="2">
+      <div className="nav bg-black h-10 text-white w-full flex justify-end align-baseline items-center border-b">
+        <div className="flex items-center justify-center">
           {/* setting icon */}
 
-          <button className="mr-2 focus:outline-none">
+         <div>
+         <button className="mr-2 focus:outline-none">
             <i className="fas fa-cog"></i>
           </button>
+         </div>
 
           {/* notification icon */}
-          <button className="mr-2 focus:outline-none">
+     <div>
+     <button className="mr-2 focus:outline-none">
             <i className="fas fa-bell"></i>
           </button>
+     </div>
 
           {/* usericon with dropdown */}
-          <button
+          {/* <button
             className="mr-2 focus:outline-none"
             onClick={handleUserIconClick}
           >
             <i className="fas fa-user"></i>
-          </button>
+            <img src={profileImage} alt="" />
+          </button> */}
+
+          <div className="bg-white rounded-full h-8 w-8" onClick={handleUserIconClick}>
+            <img src={profileImage} alt="" className="h-full w-full rounded-full object-cover"
+            />
+          </div>
           {showOptions && (
             <div className="absolute right-2 top-8 mt-2 bg-violet-600 rounded shadow-lg">
             <Link to = "/profile">
